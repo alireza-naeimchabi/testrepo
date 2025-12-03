baf20259101ab53a9526e62a3cf3bb138d9b2095
alireza-naeimchabi <alireza.kawbi@gmail.com>
2025-12-03 09:10:12 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        double x[] = {16, 17, 18, 15, 20, 16, 13, 12, 19};
+        
+        for (int i = 0; i < x.length; i++) {
+            System.out.format("x%d = %f\n", i, x[i]);
+        }
+    }
+}
+                             
+     
